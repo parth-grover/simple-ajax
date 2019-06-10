36a7bc8fedd5eade44f710ade4093c6fbc895d43
Parth Grover <parth.grover@buildingblocks.in>
2019-06-10 17:28:54 +0530

    update ajax framework

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <html>
 <head>
+	<title>Ajax Framework</title>
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
@@ -19,11 +20,29 @@
 			<label for="exampleInputPassword1">Password</label>
 			<input type="password" class="form-control" name="password" id="password" placeholder="Password">
 		  </div>
+		  <div class="form-group">
+			<label for="exampleInputPassword1">Phone</label>
+			<input type="text" class="form-control" name="phone" id="phone" placeholder="Phone">
+		  </div>
+		  <div class="form-check">
+			<input type="checkbox" class="form-check-input" id="check" name="check">
+			<label class="form-check-label" for="exampleCheck1">Check me out</label>
+		  </div>
 		  <button type="submit" class="btn btn-primary">Submit</button>
 		  <div id="form_success_msg"></div>
 		</form>
-	</div>
+	
 
+	
+		<h1 class="mt-5">Some points to consider</h1>
+		<ul class="list-group">
+		  <li class="list-group-item">Make sure input type has <b>name attribute</b></li>
+		  <li class="list-group-item">Use <b>"ajax_form_post"</b> with form id for whole form data</li>
+		  <li class="list-group-item">Use <b>"ajax_post"</b> for particulars Values</li>
+		  <li class="list-group-item">ajax_form_post Syntex <b>"ajax_form_post('http://localhost/test_ajax/script','getList',$("#contact_form").serializeArray());"</b></li>
+		  <li class="list-group-item">ajax_post Syntex <b>"ajax_post('getList',JSON.stringify({hello:'hello',hello1:'hello1',hello2:'hello2'}));"</b></li>
+		</ul>
+	</div>
 </body>
 
 
