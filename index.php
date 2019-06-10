df6f286032a9697b286b8249165380355772cc44
Parth Grover <parth.grover@buildingblocks.in>
2019-06-10 18:03:02 +0530

    update

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
 	<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.5.1/jquery.min.js"></script>
-	<script type="text/javascript" src="http://localhost/test_ajax/ajax.js"></script>
+	<script type="text/javascript" src="http://localhost/simple-ajax/ajax.js"></script>
 </head>
 <body>
 	<div class="container my-5">
